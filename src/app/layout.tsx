221a4f77b83142dd339ab7f2764f3acc0b5d38a1
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-14 16:24:30 -0300

    feat: configure app fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono as GeistMono } from 'next/font/google'
+import { Bai_Jamjuree as BaiJamjuree, Roboto } from 'next/font/google'
 
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const roboto = Roboto({
   subsets: ['latin'],
+  variable: '--font-roboto',
 })
 
-const geistMono = GeistMono({
-  variable: '--font-geist-mono',
+const baiJamjuree = BaiJamjuree({
   subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
 })
 
 export const metadata: Metadata = {
@@ -24,11 +25,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="flex min-h-full flex-col">{children}</body>
+    <html lang="en">
+      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans`}>
+        {children}
+      </body>
     </html>
   )
 }
