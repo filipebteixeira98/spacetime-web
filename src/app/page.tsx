b585f0ff52a347af1d92f7abfda193bd4b26b793
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-17 13:27:43 -0300

    feat(home): render profile for authenticated users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
+import { cookies } from 'next/headers'
+
 import { Copyright } from '@/src/components/Copyright'
 import { EmptyMemories } from '@/src/components/EmptyMemories'
 import { Hero } from '@/src/components/Hero'
+import { Profile } from '@/src/components/Profile'
 import { SignIn } from '@/src/components/SignIn'
 
-export default function Home() {
+export default async function Home() {
+  const isAuthenticated = (await cookies()).has('token')
+
   return (
     <main className="grid min-h-screen grid-cols-2">
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
         <div className="blur-full absolute top-1/2 right-0 h-72 w-131.5 translate-x-1/2 -translate-y-1/2 rounded-full bg-purple-700 opacity-50" />
         <div className="bg-stripes absolute top-0 right-2 bottom-0 w-2" />
-        <SignIn />
+        {isAuthenticated ? <Profile /> : <SignIn />}
         <Hero />
         <Copyright />
       </div>
