d7582f17f992c65cbfaae59da49203aeab4fb0aa
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-07-17 13:26:41 -0300

    feat(lib): add authenticated user helper

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { jwtDecode } from 'jwt-decode'
+import { cookies } from 'next/headers'
+
+interface User {
+  sub: string
+  name: string
+  avatarUrl: string
+}
+
+export async function getUser(): Promise<User> {
+  const token = (await cookies()).get('token')?.value
+
+  if (!token) {
+    throw new Error('Unauthorized.')
+  }
+
+  const user: User = jwtDecode(token)
+
+  return user
+}
